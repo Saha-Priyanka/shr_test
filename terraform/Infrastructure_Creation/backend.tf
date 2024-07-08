153df05d7f88642bdd56f8d0a05cb2c97f6a78be
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-08 19:44:19 +0530

    Update backend.tf

--- terraform/Infrastructure_Creation/backend.tf
+++ terraform/Infrastructure_Creation/backend.tf
@@ -2,7 +2,7 @@ terraform {
     # required_version = "0.14.8"
     backend "azurerm" {
         resource_group_name = "rg-tfstatefile-shr-dev-14"
-        storage_account_name = "sttfstateshrdev16"
+        storage_account_name = "sttfstateshrdev17"
         container_name = "tfstate-shr-dev-14"
         key = "terraform.tfstate"
        # arm_subscription_id = "793729a4-a745-4e4f-8b8d-88bda288a86a"
