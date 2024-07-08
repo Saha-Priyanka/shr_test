a208d68041a2fb4884f0c37c97c591a3046ffedd
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-08 22:29:51 +0530

    Update main.tf

--- terraform2/Infrastructure_Creation2/modules/container/main.tf
+++ terraform2/Infrastructure_Creation2/modules/container/main.tf
@@ -2,7 +2,7 @@
 
 
 data "azurerm_storage_account" "storage" {
-  name                     = "sttfstateshrdev18"
+  name                     = "sttfstateshrdev19"
   resource_group_name      = "rg-tfstatefile-shr-dev-14"
  
 }
@@ -10,7 +10,7 @@ data "azurerm_storage_account" "storage" {
 
 
 resource "azurerm_storage_container" "container" {
-  name                  = "tfstate-shr-dev-18"
+  name                  = "tfstate-shr-dev-19"
   storage_account_name  = data.azurerm_storage_account.storage.name
   container_access_type = "private"
 
