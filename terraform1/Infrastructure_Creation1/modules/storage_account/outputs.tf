77d512caeafdd9d40a92612e4e2176d4bdffb3e3
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-08 18:14:18 +0530

    Update outputs.tf

--- terraform1/Infrastructure_Creation1/modules/storage_account/outputs.tf
+++ terraform1/Infrastructure_Creation1/modules/storage_account/outputs.tf
@@ -1,14 +1,14 @@
 output "storage_id" {
-  value = data.azurerm_storage_account.storage.id
+  value = azurerm_storage_account.storage.id
 }
 output "storage_name" {
-  value = data.azurerm_storage_account.storage.name
+  value = azurerm_storage_account.storage.name
   
 }
 output "storage_primary_access_key" {
-  value = data.azurerm_storage_account.storage.primary_access_key
+  value = azurerm_storage_account.storage.primary_access_key
 }
 
 output "storage_fs_connection_string" {
-  value = data.azurerm_storage_account.storage.primary_connection_string
+  value = azurerm_storage_account.storage.primary_connection_string
 }
