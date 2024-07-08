cd2bef7fa8d6ed1f45e88d08c1fc347da18eab8d
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-08 19:43:55 +0530

    Update main.tf

--- terraform1/Infrastructure_Creation1/modules/storage_account/main.tf
+++ terraform1/Infrastructure_Creation1/modules/storage_account/main.tf
@@ -116,15 +116,15 @@ resource "null_resource" "previous" {
 }
 
 
-resource "time_sleep" "wait_420_sec" {
+resource "time_sleep" "wait_960_sec" {
   depends_on = [null_resource.previous]
 
-  create_duration = "420s"
+  create_duration = "960s"
 }
 
-# This resource will create (at least) 30 seconds after null_resource.previous
+# This resource will create (at least) 960 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_420_sec]
+  depends_on = [time_sleep.wait_960_sec]
 }
 
 
