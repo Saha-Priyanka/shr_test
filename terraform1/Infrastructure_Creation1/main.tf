4e2ed22f0daeccf218e8e875891af7fe7398ad9f
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-08 09:40:19 +0000

    Update main.tf

--- terraform1/Infrastructure_Creation1/main.tf
+++ terraform1/Infrastructure_Creation1/main.tf
@@ -19,7 +19,7 @@ locals {
 # resource group modularaisation
 module "azurerm_resource_group" {
   source              = "./modules/resource_group"
-  resource_group_name = "rg-tfstatefile-shr-dev-08"
+  resource_group_name = "rg-tfstatefile-shr-dev-09"
   location            = "France Central"
 }
 
@@ -168,7 +168,7 @@ module "key_vault" {
 module "azurerm_storage_account" {
   source                   = "./modules/storage_account"
   depends_on               = [data.azurerm_subnet.subnet_pep]
-  storage_account_name     = "sttfstateshrdev08"
+  storage_account_name     = "sttfstateshrdev09"
   resource_group_name      = module.azurerm_resource_group.resource_group_name
   location                 = module.azurerm_resource_group.resource_group_location
   account_replication_type = "LRS"
