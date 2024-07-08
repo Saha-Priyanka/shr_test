7219d8c306501a09fe446e42bf5bafb73e1aae7d
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-08 21:39:24 +0530

    Update variables.tf

--- terraform2/Infrastructure_Creation2/modules/container/variables.tf
+++ terraform2/Infrastructure_Creation2/modules/container/variables.tf
@@ -3,49 +3,21 @@ variable "resource_group_name" {
   type    = string
  
 }
- variable "resource_group_name1" {
-  type    = string
- 
-}
-variable "location" {
-  type    = string
- 
-}
+
+
 variable "storage_account_name" {
   type    = string
 }
  
-
-
-
-
-variable "account_replication_type" {
-  type = string
-}
-
-variable "account_kind" {
-  type = string
+variable "container_name" {
+  type    = string
 }
 
 
 
-variable "endpoint_name" {
-  type = string
-}
-
-variable "service_connection_name" {
+variable "access_type" {
   type = string
 }
 
 
-
-variable "vnet_name" {
-  type = string
-}
-
-
-
-variable "snet_name" {
-  type = string
-}
 
