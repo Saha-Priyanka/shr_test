868149c6840a321e8ff45301289c127dba37f75e
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-09 18:28:23 +0530

    Update main.tf

--- terraform2/Infrastructure_Creation2/modules/container/main.tf
+++ terraform2/Infrastructure_Creation2/modules/container/main.tf
@@ -11,7 +11,7 @@ data "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = var.container_name
-  storage_account_name  = data.azurerm_storage_account.storage.name
+  storage_account_name  = var.storage_account_name
   container_access_type = "private"
 
 }
