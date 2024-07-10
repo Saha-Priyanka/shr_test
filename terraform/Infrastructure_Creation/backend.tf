6d9a3534e7c990349b305217b1b279ead40b2485
PRIYANKA SAHA <114468969+Saha-Priyanka@users.noreply.github.com>
2024-07-10 22:36:21 +0530

    Update backend.tf

--- terraform/Infrastructure_Creation/backend.tf
+++ terraform/Infrastructure_Creation/backend.tf
@@ -1,10 +1,10 @@
 terraform {
     # required_version = "0.14.8"
     backend "azurerm" {
-        resource_group_name = "rg-layfast-ppr-14"
-        storage_account_name = "__storage_acc_name__" #"stlayfastppr01"  
-        container_name = "contlayfastppr01"
-        key = "pprterraform.tfstate"
+        resource_group_name = "__rg_name__"      #"rg-layfast-ppr-14"
+        storage_account_name = "__storage_acc_name__"      #"stlayfastppr01"  
+        container_name = "__cont_name__"     #"contlayfastppr01"
+        key = "__tfstate_file__"     #"pprterraform.tfstate"
        # arm_subscription_id = "793729a4-a745-4e4f-8b8d-88bda288a86a"
        # arm_tenant_id = "1e9cc706-c3fd-4b8c-9dbd-a073e7384b56"
         
